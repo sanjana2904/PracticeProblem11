ee1262e1f485b97c2d8259194bd29810d70deeac
sanjana k <sanjanarthi@gmail.com>
2021-03-17 19:25:49 +0530

    "Welcome to User Registration Problem"

--- PracticeProblem11/Program.cs
+++ PracticeProblem11/Program.cs
@@ -6,7 +6,7 @@ namespace PracticeProblem11
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to User Registration Problem");
         }
     }
 }
