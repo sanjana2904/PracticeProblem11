ac66c1f5cf9128117229b311c3e28eb0330d4011
sanjana k <sanjanarthi@gmail.com>
2021-03-17 20:23:57 +0530

    "Added UC3 - Exception"

--- PracticeProblem11/Program.cs
+++ PracticeProblem11/Program.cs
@@ -16,6 +16,17 @@ namespace PracticeProblem11
             return result;
         }
 
+        public bool validateEmail(string name)
+        {
+            string pattern = @"^[\w]+@[a-zA-Z0-9]+.[a-zA-Z0-9]+";
+            bool result = Regex.IsMatch(name, pattern);
+            if (!result)
+            {
+                throw new UserException("Email Name Validation Failed");
+            }
+            return result;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Problem");
@@ -32,7 +43,18 @@ namespace PracticeProblem11
                 Console.WriteLine("Exception caught");
             }
 
+            try
+            {
+                result = program.validateName("sanju@gmail.com");
+            }
+            catch (UserException)
+            {
+                Console.WriteLine("Exception caught in Email");
+            }
+
             Console.WriteLine("First name validation result: " + result);
+            Console.WriteLine("Email name validation result: " + result);
         }
     }
 }
+
